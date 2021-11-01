ee61e1e4fe95ecbaca3b4b5aa81c026cdf7df417
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2021-10-31 22:20:54 -0600

    Update predictPhonemes.py

--- predictPhonemes.py
+++ predictPhonemes.py
@@ -15,7 +15,7 @@ from util.pad import audiovisual_batch_collate
 from util.seq_alignment import beam_search
 
 #Get a full list of all videos with speakers, sentences, and offsets
-per_ckpt = "/home/lukas/Dev/speech_cpc/lrs2_audiovisual_lstm_phoneme_classifier_lightning_logs/lightning_logs/version_0/checkpoints/epoch=10-step=63018.ckpt"
+per_ckpt = "/home/analysis/Dev/lukas/speech_cpc/lrs2_audio_only_lightning_logs/lightning_logs/version_0/checkpoints/epoch=60-step=1083908.ckpt"
 offsetMap={
 	0:"I840",
 	1:"I720",
