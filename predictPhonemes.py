f12b9291594533e31e7677d83cee1e14f23fafd1
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-06-28 00:18:50 -0600

    Update predictPhonemes.py

--- predictPhonemes.py
+++ predictPhonemes.py
@@ -65,7 +65,7 @@ def createDatasetPaths():
 
 #Get a full list of all videos with speakers, sentences, and offsets
 per_ckpt = "/home/analysis/Documents/studentHDD/chris/lightning_logs/version_0/checkpoints/epoch=10-step=63018.ckpt"
-dest_csv="~/Documents/studentHDD/chris/predicitiveCodingExperiment/predicitiveCodingPhonemeResults.csv"
+dest_csv="~/Documents/studentHDD/chris/predictiveCodingExperiment/predictiveCodingPhonemeResults.csv"
 datasetPath="/home/analysis/Documents/studentHDD/chris/monoSubclips/"
 
 model = FBAudioVisualCPCPhonemeClassifierLightning(src_checkpoint_path=per_ckpt, batch_size=1, cached=False, LSTM=True).cuda()
@@ -123,6 +123,6 @@ for index, data in tqdm(enumerate(testGenerator), total=len(testGenerator)):
 		#nItems += 1
 
 #print(resultArr)
-with open("PER.csv","w") as file:
+with open(dest_csv,"w") as file:
 	writer=csv.writer(file,delimiter=',')
 	writer.writerows(resultArr)
